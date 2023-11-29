ae331c8282fb83b91f88016072eb33d41630fb10
Willem Kaufmann <willem.kaufmann@gmail.com>
2023-11-29 18:27:55 -0500

    Updated

--- 2017/solns/25.cpp
+++ 2017/solns/25.cpp
@@ -21,10 +21,19 @@ struct day_25 : public Advent_type {
   static constexpr int numSteps  = 12629077;
   static constexpr int numStates = 6;
 
+  enum class StateType : std::size_t {
+    A = 0,
+    B,
+    C,
+    D,
+    E,
+    F
+  };
+
   struct Action {
     uint8_t writeValue;
     int8_t moveDir;
-    int nextState;
+    StateType nextState;
   };
 
   struct State {
@@ -38,9 +47,12 @@ struct day_25 : public Advent_type {
   };
 
   static constexpr std::array<State, numStates> stateMap = {
-      State{Action{1, 1, 1},  Action{0, -1, 1}}, State{Action{0, 1, 2},  Action{1, -1, 1}},
-      State{Action{1, 1, 3},  Action{0, -1, 0}}, State{Action{1, -1, 4}, Action{1, -1, 5}},
-      State{Action{1, -1, 0}, Action{0, -1, 3}}, State{Action{1, 1, 0},  Action{1, -1, 4}}};
+      State{Action{1, 1, StateType::B}, Action{0, -1, StateType::B}},
+      State{Action{0, 1, StateType::C}, Action{1, -1, StateType::B}},
+      State{Action{1, 1, StateType::D}, Action{0, -1, StateType::A}},
+      State{Action{1, -1, StateType::E}, Action{1, -1, StateType::F}},
+      State{Action{1, -1, StateType::A}, Action{0, -1, StateType::D}},
+      State{Action{1, 1, StateType::A}, Action{1, -1, StateType::E}}};
 
   std::string part_1() override {
     std::unordered_map<int, uint8_t> tape;
@@ -52,7 +64,7 @@ struct day_25 : public Advent_type {
       const auto& a = (c == 0) ? s.zeroAction : s.oneAction;
       c             = a.writeValue;
       p += a.moveDir;
-      s = stateMap.at(a.nextState);
+      s = stateMap[std::to_underlying(a.nextState)];
     }
 
     return std::to_string(std::accumulate(tape.begin(), tape.end(), 0,

45d16533c9f19cc260b7e99c236ab49fd80e6b36
Willem Kaufmann <willem.kaufmann@gmail.com>
2023-12-08 00:51:51 -0500

    minor changes

--- 2023/solns/08.cpp
+++ 2023/solns/08.cpp
@@ -48,11 +48,10 @@ struct day_08 : public Advent_type {
 
     auto state                          = State{"AAA", 0};
     static constexpr Node_type goalNode = "ZZZ";
-    while (state.node != goalNode) {
+    for (; state.node != goalNode; ++state.numSteps) {
       const auto& [l, r] = graph.at(state.node);
       const auto& LR     = instruct[state.numSteps % instruct.size()];
       state.node         = (LR == 'L') ? l : r;
-      ++state.numSteps;
     }
 
     return std::to_string(state.numSteps);
@@ -69,15 +68,16 @@ struct day_08 : public Advent_type {
     };
 
     std::vector<Node_type> start;
-    for (const auto& [k, v] : graph) {
-      if (k.back() == 'A') {
-        start.push_back(k);
+    for (const auto& [node, lr] : graph) {
+      if (node.back() == 'A') {
+        start.push_back(node);
       }
     }
 
     auto state = State(start, 0);
-    while (
-        !(std::all_of(state.LCM.begin(), state.LCM.end(), [](const auto& i) { return i != -1; }))) {
+    for (;
+         !(std::all_of(state.LCM.begin(), state.LCM.end(), [](const auto& i) { return i != -1; }));
+         ++state.numSteps) {
       for (auto [index, node] : std::views::enumerate(state.nodes)) {
         if (node.back() == 'Z') {
           state.LCM[index] = state.numSteps;
@@ -86,7 +86,6 @@ struct day_08 : public Advent_type {
         const auto& LR     = instruct[state.numSteps % instruct.size()];
         node               = (LR == 'L') ? l : r;
       }
-      ++state.numSteps;
     }
 
     return std::to_string(Algorithm::LCM(state.LCM));
